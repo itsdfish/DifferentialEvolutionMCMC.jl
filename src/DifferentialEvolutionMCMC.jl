001a6d5d5dee3b14b478e0d97050807cb8cc8d28
itsdfish <itsdfish@gmail.com>
2020-05-28 07:06:08 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.1

---
+++ src/DifferentialEvolutionMCMC.jl
@@ -0,0 +1,5 @@
+module DifferentialEvolutionMCMC
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using DifferentialEvolutionMCMC
+using Test
+
+@testset "DifferentialEvolutionMCMC.jl" begin
+    # Write your tests here.
+end
