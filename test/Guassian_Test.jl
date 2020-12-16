44d01ec0f4cacd474de49278f1570a421bf4963a
itsdfish <itsdfish@gmail.com>
2020-12-16 14:32:57 -0500

    add safetestsets and sequentialsamplingmodels

---
+++ test/Guassian_Test.jl
@@ -0,0 +1,38 @@
+using DifferentialEvolutionMCMC, Test, Random, Turing, Parameters, Distributions
+import DifferentialEvolutionMCMC: select_groups, select_particles, shift_particles!, sample_init
+Random.seed!(973536)
+priors = (
+    μ = (Normal(0, 10),),
+    σ = (truncated(Cauchy(0, 1), 0.0, Inf),)
+)
+
+bounds = ((-Inf,Inf),(0.0,Inf))
+
+data = rand(Normal(0.0, 1.0), 100)
+
+function loglike(μ, σ, data)
+    return sum(logpdf.(Normal(μ, σ), data))
+end
+
+model = DEModel(priors=priors, model=loglike, data=data)
+de = DE(;priors=priors, bounds=bounds, burnin=1500)
+n_iter = 3000
+chains = sample(model, de, n_iter)
+μ_de = describe(chains)[1][:,:mean]
+σ_de = describe(chains)[1][:,:std]
+rhat = describe(chains)[1][:,:rhat]
+
+@model model1(data) = begin
+    μ ~ Normal(0, 10)
+    σ ~ truncated(Cauchy(0, 1), 0.0, Inf)
+    for i in 1:length(data)
+        data[i] ~ Normal(μ, σ)
+    end
+end
+chn = sample(model1(data), NUTS(1000, .85), 2000)
+μ_nuts = describe(chn)[1][:,:mean]
+σ_nuts = describe(chn)[1][:,:std]
+
+@test all(isapprox.(rhat, fill(1.0, 2), atol=.05))
+@test all(isapprox.(μ_nuts, μ_de, atol=.01))
+@test all(isapprox.(σ_nuts, σ_de, atol=.01))
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,90 @@
-using DifferentialEvolutionMCMC, Test
-cd(@__DIR__)
-@testset "DE-MCMC Tests" begin
-    include("DEMCMC_Tests.jl")
+using SafeTestsets
+
+@safetestset "Binomial Model" begin
+    using DifferentialEvolutionMCMC, Test, Random, Turing, Parameters, Distributions
+    import DifferentialEvolutionMCMC: select_groups, select_particles, shift_particles!, sample_init
+    Random.seed!(29542)
+    N = 10
+    k = rand(Binomial(N, .5))
+    data = (N = N,k = k)
+    priors = (
+        θ = (Beta(1, 1),),
+    )
+
+    bounds = ((0,1),)
+
+    function loglike(θ, data)
+        return logpdf(Binomial(data.N, θ), data.k)
+    end
+
+    model = DEModel(priors=priors, model=loglike, data=data)
+
+    de = DE(;priors=priors, bounds=bounds, burnin=1500)
+    n_iter = 3000
+    chains = sample(model, de, n_iter)
+    μθ = describe(chains)[1][:,:mean][1]
+    σθ = describe(chains)[1][:,:std][1]
+    rhat = describe(chains)[1][:,:rhat][1]
+    solution = Beta(k + 1, N - k + 1)
+    @test μθ ≈ mean(solution) rtol = .02
+    @test σθ ≈ std(solution) rtol = .02
+    @test rhat ≈ 1.0 atol = .01
+end
+
+@safetestset "Gaussian" begin
+    include("Guassian_Test.jl")
+end
+
+@safetestset "LNR" begin
+   include("Log_Normal_Race_Test.jl")
+end
+
+@safetestset "Migration" begin
+    using DifferentialEvolutionMCMC, Test, Random, Turing, Parameters, Distributions
+    import DifferentialEvolutionMCMC: select_groups, select_particles, shift_particles!, sample_init
+
+    Random.seed!(0451) # Random.seed!(459)
+
+    function equal(p1::Particle, p2::Particle)
+        fields = fieldnames(Particle)
+        for field in fields
+            if getfield(p1, field) != getfield(p2, field)
+                println(field)
+                return false
+            end
+        end
+        return true
+    end
+
+    priors = (
+        θ = (Beta(1, 1),),
+    )
+    bounds = ((0,1),)
+
+    data = (N = 10,k = 5)
+
+    function loglike(θ, data)
+        return logpdf(Binomial(data.N, θ), data.k)
+    end
+
+    model = DEModel(priors=priors, model=loglike, data=data)
+    de = DE(;priors=priors, bounds=bounds, burnin=1500)
+    n_iter = 3000
+    groups = sample_init(model, de, n_iter)
+    sub_group = select_groups(de, groups)
+    c_groups = deepcopy(groups)
+    c_sub_group = deepcopy(sub_group)
+    p_idx,particles = select_particles(sub_group)
+    c_particles = deepcopy(particles)
+    shift_particles!(sub_group, p_idx, particles)
+    gidx = 1:length(sub_group)
+    cidx = circshift(gidx, 1)
+    cp_idx = circshift(p_idx, 1)
+    for (i,c,p,cp) in zip(gidx, p_idx, cidx, cp_idx)
+        @test sub_group[i][c].Θ == c_sub_group[p][cp].Θ
+    end
+    ridx = [4,3] # ridx = [4,1]
+    for (i,c,r) in zip(1:2, p_idx[1:2], ridx)
+        @test sub_group[i][c].Θ == groups[r][c].Θ
+    end
 end
