855a9a95cfc74df9d3d436f5111e01c38f0ef7a4
itsdfish <itsdfish@gmail.com>
2021-11-05 06:38:05 -0400

    update examples

--- Examples/Hierarchical_Example.jl
+++ Examples/Hierarchical_Example.jl
@@ -1,16 +1,13 @@
+###################################################################################
+#                             load dependencies
+###################################################################################
 cd(@__DIR__)
 using DifferentialEvolutionMCMC, Random, Distributions
-
 Random.seed!(9528)
-
-bounds = (
-    (-Inf,Inf),
-    (-Inf,Inf),
-    (0.0,Inf),
-    (-Inf,Inf),
-    (0.0,Inf),
-)
-
+###################################################################################
+#                             define functions
+###################################################################################
+# returns prior log likelihood
 function sample_prior()
     LL = 0.0
     β0 = rand(Normal(1, 1))
@@ -21,6 +18,7 @@ function sample_prior()
     return as_union([β0, β1, σβ0, β0i, σ])
 end
 
+# function for initial values
 function prior_loglike(β0, β1, σβ0, β0i, σ)
     LL = 0.0
     LL += logpdf(Normal(0, 1), β0)
@@ -31,6 +29,7 @@ function prior_loglike(β0, β1, σβ0, β0i, σ)
     return LL
 end
 
+# likelihood function 
 function loglike(data, x, β0, β1, σβ0, β0i, σ)
     LL = 0.0
     for s in 1:length(data)
@@ -39,7 +38,9 @@ function loglike(data, x, β0, β1, σβ0, β0i, σ)
     end
     return LL
 end
-
+###################################################################################
+#                             generate data
+###################################################################################
 # number of responses 
 n_data = 100
 # number of subjects 
@@ -57,9 +58,22 @@ n_subj = 50
 x = rand(Normal(0, 1), n_subj)
 linear(x, β0, β0i, β1, σ, n_data) = β0 + β0i + β1 * x  .+ rand(Normal(0, σ), n_data)
 data = [linear(x[i], β0, β0i[i], β1, σ, n_data) for i in 1:n_subj]
+###################################################################################
+#                             configure sampler
+###################################################################################
+# parameter bounds
+bounds = (
+    (-Inf,Inf),
+    (-Inf,Inf),
+    (0.0,Inf),
+    (-Inf,Inf),
+    (0.0,Inf),
+)
 
+# parameter names
 names = (:β0, :β1, :σβ0, :β0i, :σ)
 
+# model object 
 model = DEModel(x; 
     sample_prior, 
     prior_loglike, 
@@ -68,6 +82,7 @@ model = DEModel(x;
     names
 )
 
+# sampler object
 de = DE(;
     bounds, 
     sample = resample,
@@ -77,6 +92,8 @@ de = DE(;
     n_groups = 2,
     θsnooker = 0.1
 )
-
+###################################################################################
+#                             estimate parameters
+###################################################################################
 n_iter = 50_000
 chains = sample(model, de, MCMCThreads(), n_iter, progress=true)
