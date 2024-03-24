d6edf15eb8bb673ddfbd8b2b11019e0d90d6f8ae
itsdfish <itsdfish@gmail.com>
2024-03-24 19:19:51 -0400

    add autoformatter

--- test/lognormal_race_tests.jl
+++ test/lognormal_race_tests.jl
@@ -1,34 +1,33 @@
-using DifferentialEvolutionMCMC, Test, Random, Turing, Parameters, Distributions
+using DifferentialEvolutionMCMC, Test, Random, Parameters, Distributions
 using SequentialSamplingModels, LinearAlgebra
+using Turing 
 Random.seed!(9918)
 
-dist = LNR(μ=[-2.,-2.,-3.,-3], σ=1.0, ϕ=.5)
+dist = LNR(ν=[-2.,-2.,-3.,-3], σ=fill(1.0, 4), τ=.5)
 data = rand(dist, 100)
 
-function loglike(data, μ, σ, ϕ)
-    dist = LNR(μ, σ, ϕ)
-    return sum(logpdf.(dist, data))
+function loglike(data, ν, τ)
+    dist = LNR(;ν, τ)
+    return sum(logpdf(dist, data))
 end
 
-function prior_loglike(μ, σ, ϕ)
+function prior_loglike(ν, τ)
     LL = 0.0
-    LL += sum(logpdf.(Normal(0, 3), μ))
-    LL += logpdf(truncated(Cauchy(0, 1), 0.0, Inf), σ)
-    LL += logpdf(Uniform(0., min_rt), ϕ)
+    LL += sum(logpdf.(Normal(0, 3), ν))
+    LL += logpdf(Uniform(0., min_rt), τ)
 end
 
 function sample_prior()
     LL = 0.0
-    μ = rand(Normal(0, 3), 4)
-    σ = rand(truncated(Cauchy(0, 1), 0.0, Inf))
-    ϕ = rand(Uniform(0, min_rt))
-    return as_union([μ,σ,ϕ])
+    ν = rand(Normal(0, 3), 4)
+    τ = rand(Uniform(0, min_rt))
+    return as_union([ν, τ])
 end
 
 min_rt = minimum(x -> x[2], data)
 
-bounds = ((-Inf,0.),(1e-10,Inf),(0.,min_rt))
-names = (:μ,:σ,:ϕ)
+bounds = ((-Inf,Inf),(0.,min_rt))
+names = (:ν, :τ)
 
 model = DEModel(; 
     sample_prior, 
@@ -53,10 +52,9 @@ loglikelihood(d::LNR, data::Vector{<:Tuple}) = sum(logpdf.(d, data))
 
 @model model1(data) = begin
     min_rt = minimum(x -> x[2], data)
-    μ ~ MvNormal(zeros(4), I * 3^2)
-    σ ~ truncated(Cauchy(0, 1), 0.0, Inf)
-    ϕ ~ Uniform(0.0, min_rt)
-    data ~ LNR(μ=μ, σ=σ, ϕ=ϕ)
+    ν ~ MvNormal(zeros(4), I * 3^2)
+    τ ~ Uniform(0.0, min_rt)
+    data ~ LNR(;ν, τ)
 end
 
 Random.seed!(68541)
@@ -64,6 +62,6 @@ chn = sample(model1(data), NUTS(1500, .85), 4000)
 μ_nuts = describe(chn)[1][:,:mean]
 σ_nuts = describe(chn)[1][:,:std]
 
-@test all(isapprox.(rhat, fill(1.0, 6), atol=.05))
+@test all(isapprox.(rhat, fill(1.0, 5), atol=.05))
 @test all(isapprox.(μ_nuts, μ_de, rtol=.05))
 @test all(isapprox.(σ_nuts, σ_de, rtol=.05))
